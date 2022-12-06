e8460caae9d0c774bf44f4253aa147ee0e6e8bd7
Jared Grimes <jaredlgrimes@hotmail.com>
2022-12-06 15:12:17 -0500

    animation!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,48 @@
   /** @type {import('./$types').PageData} */
   export let form;
   import { enhance } from '$app/forms';
+
+  let clicked = false;
 </script>
 
-<form method="POST" use:enhance>
-  <input name="card" />
-  <button>search</button>
-</form>
+<div class={clicked ? 'card-search-input__clicked' : 'card-search-input__not-clicked'}>
+  <form method="POST" use:enhance>
+    <input name="card" />
+    <button on:click={() => clicked = true}>search</button>
+  </form>
+</div>
 
-{JSON.stringify(form?.cards)}
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="card-list">
+  {#each form?.cards ?? [] as card}
+    <img class="card" src={card.images.small} />  
+  {/each}
+</div>
+
+<style>
+  .card-list {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .card {
+    width: 47vw;
+  }
+
+  .card-search-input__not-clicked {
+    margin-top: 50%;
+  }
+
+  .card-search-input__clicked {
+    animation-duration: 0.75s;
+    animation-name: slidein;
+  }
+
+  @keyframes slidein {
+    from {
+      margin-top: 50%;
+    }
+
+    to {
+     margin-top: 0;
+    }
+  }
+</style>
