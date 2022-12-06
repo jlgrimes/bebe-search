a0ffc289ac98c9996ec49f4db1c0e4a554147ea8
Jared Grimes <jaredlgrimes@hotmail.com>
2022-12-06 14:56:28 -0500

    changed to use api

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,16 +1,15 @@
+import { PokemonTCG } from "pokemon-tcg-sdk-typescript";
+
 /** @type {import('./$types').Actions} */
 export const actions = {
   default: async ({ request }: { request: Request }) => {
     const formData = await request.formData();
     const cardName = formData.get('card');
-    console.log(cardName)
 
-    const res = await fetch(`https://api.pokemontcg.io/v2/cards?q=name:${cardName}`, {
-      headers: {
-        'Content-Type': 'application/json',
-      }
+    const cards = await PokemonTCG.findCardsByQueries({
+      q: `name:${cardName}`
     });
-    const cards = await res.json();
+
     return {
       cards
     };
