2840f4d9acf3ba6730a97af442be6040216ab4de
Jared Grimes <jaredlgrimes@hotmail.com>
2022-12-06 16:16:11 -0500

    can search multiple words

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,15 +3,19 @@ import { PokemonTCG } from "pokemon-tcg-sdk-typescript";
 /** @type {import('./$types').Actions} */
 export const actions = {
   default: async ({ request }: { request: Request }) => {
-    const formData = await request.formData();
-    const cardName = formData.get('card');
-
-    const cards = await PokemonTCG.findCardsByQueries({
-      q: `name:${cardName}`
-    });
-
-    return {
-      cards
-    };
+    try {
+      const formData = await request.formData();
+      const cardName = formData.get('card') as string;
+  
+      const cards = await PokemonTCG.findCardsByQueries({
+        q: `name:"${cardName}"`
+      });
+  
+      return {
+        cards
+      };
+    } catch(err) {
+      console.log(err);
+    }
   }
 };
