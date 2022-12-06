82ab346e9db3af7ded9869b0967fa57f7091b206
Jared Grimes <jaredlgrimes@hotmail.com>
2022-12-06 16:28:56 -0500

    bouncy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
   export let form;
   import { enhance } from '$app/forms';
   import { ButtonGroup, Input, ListPlaceholder, Button } from 'flowbite-svelte';
+  import 'animate.css';
 
   let clicked = false;
   let selectedCardUrl: string;
@@ -37,7 +38,7 @@
 </div>
 
 {#if selectedCardUrl}
-  <img class='selected-card' src={selectedCardUrl} on:click={() => selectedCardUrl = null} />
+  <img class='selected-card animate__animated animate__bounceIn' src={selectedCardUrl} on:click={() => selectedCardUrl = null} />
   <div class="selected-card-overlay" on:click={() => selectedCardUrl = null}></div>
 {/if}
 
